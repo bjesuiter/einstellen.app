2be5ea6f19bf3ef8147107834588eb8bc645550b
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-09-17 00:15:18 +0200

    fix scrolling in login layout

--- src/components/layout/LoginLayout.tsx
+++ src/components/layout/LoginLayout.tsx
@@ -8,7 +8,7 @@ export  const LoginLayout = ({ title, clerkButtonClasses }: { title?: string, cl
     );
   
     return (
-        <div class="flex flex-col justify-center items-center h-full">
+        <div class="flex flex-col justify-center-safe items-center-safe h-full">
             <div class="shadow-xl shadow-primary-dark/20 rounded-4xl border-solid border-primary/40 dark:border-primary-dark/40 border-2 bg-surface-main-light dark:bg-surface-main-dark text-foreground-light dark:text-foreground-dark flex flex-col gap-4 pb-6 transition-all duration-200">
                 <SineWave className="mt-2 rounded-4xl" />
                 {titleElement}
