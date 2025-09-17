8b9ca216a6314291a60ac7f85908bb4063dc4e5e
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-09-17 12:38:07 +0200

    improve wsPong response

--- src/routes/-trpc/trpc-ws.router.ts
+++ src/routes/-trpc/trpc-ws.router.ts
@@ -1,7 +1,9 @@
 import { mergeRouters, publicProcedure, routerFn } from "./trpc-ws.base";
 
 const mainWsRouter = routerFn({
-    wsping: publicProcedure.query(() => "wspong"),
+    wsping: publicProcedure.query(() =>
+        "wspong at " + new Date().toISOString()
+    ),
 });
 
 // Export the full trpc router object
